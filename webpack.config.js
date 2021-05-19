b12857855bedb4c290a7689b396e417c33a34125
Jordan Lancheros <jordan1703@hotmail.es>
2021-05-18 20:23:20 -0500

    add new page

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,7 @@ module.exports = (env, options) => {
     },
     output: {
       filename: "[name].js",
-      path: path.resolve(__dirname, "js"),
+      path: path.resolve(__dirname, "docs/src/js"),
     },
     module: {
       rules: [{
